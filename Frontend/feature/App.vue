1df5e49f841a5b89e1331dd443305ae987e3d1dc
tinow04 <tino.wolter2@gmail.com>
2025-02-06 10:00:24 +0100

    changed structure of project

--- Frontend/feature/App.vue
+++ Frontend/feature/App.vue
@@ -1,14 +1,13 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import Game from './game/src/pages/Game.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <Game msg="You did it!" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
